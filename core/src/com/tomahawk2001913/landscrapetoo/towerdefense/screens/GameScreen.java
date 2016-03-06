f6a28ad8b07e43e56e42d2769296a553d4dc20a0
Tomahawk2001913 <tomahawk2001913@gmail.com>
2016-03-06 12:55:51 -0500

    Background color changed to a better shade of blue, default map changed.

--- core/src/com/tomahawk2001913/landscrapetoo/towerdefense/screens/GameScreen.java
+++ core/src/com/tomahawk2001913/landscrapetoo/towerdefense/screens/GameScreen.java
@@ -38,7 +38,7 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0, 0, 1, 1);
+		Gdx.gl.glClearColor(0.3f, 0.4f, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		batch.begin();
